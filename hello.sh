2ce2ec7ac6d312c8383d6347949bb79dc323505d
Zach <zab1211@msn.com>
2019-06-24 09:20:19 -0400

    added fav lang question

--- hello.sh
+++ hello.sh
@@ -5,3 +5,9 @@ echo Hello, what is your name?
 read name
 
 echo Hello, $name!
+echo It is a pleasure to meet you, what is your favorite programming language?
+
+read lang
+
+echo $lang is my favorite too!
+
